711d49a3891ca4cbaec2873f2870f8d613ab7181
Romzi Farhan <romzifg@gmail.com>
2021-10-01 13:52:30 +0700

    setup project react

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1 className="title">BOOK STORE</h1>
+    </div>
+  );
+};
+
+export default Navbar;
